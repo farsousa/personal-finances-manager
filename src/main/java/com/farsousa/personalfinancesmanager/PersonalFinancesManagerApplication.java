b5b04b8649da5d929a204d96dfed724021de63f4
farsousa <andersoncaj26@gmail.com>
2023-02-22 10:59:43 -0300

    estrutura inicial do projeto com as dependencias básicas

---
+++ src/main/java/com/farsousa/personalfinancesmanager/PersonalFinancesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.farsousa.personalfinancesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farsousa/personalfinancesmanager/PersonalFinancesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farsousa.personalfinancesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
