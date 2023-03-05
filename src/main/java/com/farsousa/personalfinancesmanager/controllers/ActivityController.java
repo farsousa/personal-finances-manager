58865fdd76b6249eab69826bc37bc378ef4d7a1c
farsousa <andersoncaj26@gmail.com>
2023-03-05 10:58:41 -0300

    implementado tela de detalhar lançamento e feito mascara para dinheiro e data

--- src/main/java/com/farsousa/personalfinancesmanager/controllers/ActivityController.java
+++ src/main/java/com/farsousa/personalfinancesmanager/controllers/ActivityController.java
@@ -2,17 +2,23 @@ package com.farsousa.personalfinancesmanager.controllers;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.farsousa.personalfinancesmanager.domains.entities.Activity;
 import com.farsousa.personalfinancesmanager.domains.entities.Wallet;
+import com.farsousa.personalfinancesmanager.domains.enumarations.ActivityStatus;
 import com.farsousa.personalfinancesmanager.repositories.ActivityRepository;
 import com.farsousa.personalfinancesmanager.repositories.WalletRepository;
+
+import jakarta.transaction.Transactional;
 
 @Controller
 @RequestMapping("activities")
@@ -33,6 +39,38 @@ public class ActivityController {
 		model.addAttribute("activities", activities);	
 		
 		return "activities";
+	}
+	
+	
+	@GetMapping("detail/{id}")
+	public String detail(@PathVariable Long id, Model model) {
+		Optional<Activity> activity = activityRepository.findById(id);
+		
+		if(activity.isEmpty()) {
+			return "activities";
+		}
+		
+		model.addAttribute("activity", activity.get());
+		
+		return "activity";
+	}
+	
+	@GetMapping("changeStatus/{id}")
+	@Transactional
+	public String changeStatus(@PathVariable Long id, Model model) {
+		Optional<Activity> activity = activityRepository.findById(id);
+		
+		if(activity.isEmpty()) {
+			return "activities";
+		}
+		
+		activity.get().setStatus(ActivityStatus.Efetuado);
+		activity.get().setEffectiveDate(new Date());
+		
+		
+		model.addAttribute("activity", activity.get());
+		
+		return "activity";
 	}
 
 }
